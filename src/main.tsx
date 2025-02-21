787b39e25599b8934b75593ca2cb9661369bd536
Annisa <annisaseen@gmail.com>
2025-02-21 16:20:20 +0700

    small fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,18 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />
+    }
+], {
+    future: {
+        // Remove v7_startTransition as it's not a valid option
+        v7_relativeSplatPath: true
+    }
+})
 
 createRoot(document.getElementById("root")!).render(<App />);
