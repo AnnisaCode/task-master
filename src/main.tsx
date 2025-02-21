18bda9bf01a6ec2ac60aa2de5bf2e3a973f4bec7
Annisa <annisaseen@gmail.com>
2025-02-21 18:23:57 +0700

    update hash

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,12 @@
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import App from './App.tsx'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />
-    }
-], {
-    future: {
-        // Remove v7_startTransition as it's not a valid option
-        v7_relativeSplatPath: true
-    }
-})
-
-createRoot(document.getElementById("root")!).render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+    <HashRouter>
+        <App />
+    </HashRouter>
+);
